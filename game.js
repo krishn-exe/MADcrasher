0e74185df32a4630c07dfd6afa47d667c6386efb
Krishn Sharma <183143060+krishn-exe@users.noreply.github.com>
2026-09-08 12:00:21 +0530

    add canavs

---
+++ game.js
@@ -0,0 +1,13 @@
+//Canvas setup
+
+let canvas = document.getElementById("gameCanvas");
+let ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
+
